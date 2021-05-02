a13db1ccf94b099b2d5cef5d9553bf281990b815
Phoebe <phoebe_chen@dlsu.edu.ph>
2021-05-02 21:59:25 +0800

    extract lexeme function
    hello

--- test.c
+++ test.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
- 
+#include <string.h>
 #define MAXCHAR 1000
+#define DELIM " " 
 
-void main(){
+char* get_lexeme( char* line ){
+	return strtok(line, DELIM);
+}
+
+int main(){
 	FILE *fp;
     char str[MAXCHAR];
     char* filename = "test.txt";
- 
+   	char* token = "";
+   	
     fp = fopen(filename, "r");
     
     if (fp == NULL){
@@ -14,9 +20,21 @@ void main(){
         return 1;
     }
     
-    while (fgets(str, MAXCHAR, fp) != NULL)
-        printf("%s", str);
+    //read
+    while (fgets(str, MAXCHAR, fp) != NULL){
+    	printf("line: %s\n", str);
+    	token = get_lexeme(str);
+    	//walk through tokens
+    	while( token != NULL ) {
+      		printf( "Token: %s\n", token );
+      		token = strtok(NULL, DELIM);
+   		}
+	}
+        
     fclose(fp);
     return 0;
     
 }
+
+
+//https://www.educative.io/edpresso/splitting-a-string-using-strtok-in-c
